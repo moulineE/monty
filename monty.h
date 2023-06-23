f3442fa4de1210e8468be38a6873825857ea673e
moulineE <elmahdi.mouline@live.fr>
2023-06-23 06:12:02 +0000

    0. push, pall FIX: add the header file to github

---
+++ monty.h
@@ -0,0 +1,56 @@
+#ifndef MONTY_H
+#define MONTY_H
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <sys/types.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+/**
+ * struct gl_var_s -  Encapsulation of global var in a struct
+ * @fd: a pointer to the montu opcode file
+ * arg_n: a string with the value of n
+ */
+typedef struct gl_var_s
+{
+	FILE *fd;
+	char *arg_n;
+} gl_var_t;
+extern gl_var_t gl_var;
+
+stack_t *add_dnodeint(stack_t **head, const int n);
+void execute(char *cmd, stack_t **stack, int line_number);
+void free_dlistint(stack_t *head);
+void _push(stack_t **stack, unsigned int line_number);
+void _pall(stack_t **stack, unsigned int line_number);
+
+#endif
