68f194c93260bc7894a9cce1aace69f4cb7019c7
moulineE <elmahdi.mouline@live.fr>
2023-06-23 06:13:36 +0000

    0. push, pall FIX: betty for the header file

--- monty.h
+++ monty.h
@@ -38,7 +38,7 @@ typedef struct instruction_s
 /**
  * struct gl_var_s -  Encapsulation of global var in a struct
  * @fd: a pointer to the montu opcode file
- * arg_n: a string with the value of n
+ * @arg_n: a string with the value of n
  */
 typedef struct gl_var_s
 {
