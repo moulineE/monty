671c499c89e3d9cd8988e8c5732535cf0e75e4d3
moulineE <elmahdi.mouline@live.fr>
2023-06-26 01:55:00 +0000

    Task 12. pstri FIX: simplify

--- pstr.c
+++ pstr.c
@@ -10,11 +10,6 @@ void _pstr(stack_t **stack, unsigned int line_number)
 	stack_t *read = *stack;
 	(void)(line_number);
 
-	if (read == NULL)
-	{
-		printf("\n");
-		return;
-	}
 	while (read->n != 0 && read != NULL && (read->n >= 0 && read->n <= 127))
 	{
 		printf("%c", read->n);
