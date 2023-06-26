13e82964e8bdf10b6115a73574140323a061cb13
moulineE <elmahdi.mouline@live.fr>
2023-06-26 01:58:23 +0000

    Task 12. pstri FIX: edgecase

--- pstr.c
+++ pstr.c
@@ -10,7 +10,7 @@ void _pstr(stack_t **stack, unsigned int line_number)
 	stack_t *read = *stack;
 	(void)(line_number);
 
-	while (read->n != 0 && read != NULL && (read->n >= 0 && read->n <= 127))
+	while (read != NULL && (read->n > 0 && read->n <= 127))
 	{
 		printf("%c", read->n);
 		read = read->next;
